40bee41f258acae6116b7434aa2b422c3bd66656
Wizzy019 <kingwizzyndukwu@gmail.com>
2025-10-07 23:28:57 +0100

    Added a header for the app and also Added and importes EspenseForm component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import ExpenseForm from './components/ExpenseForm'
 
 function App() {
   return (
-    <div>
-      
+    < div className="App mx-auto p-4 max-w-2xl">
+      <h1 className='heading text-4xl mx-15 my-10'>
+        Expense Handler
+        </h1>
+    <ExpenseForm/>
     </div>
   )
 }
