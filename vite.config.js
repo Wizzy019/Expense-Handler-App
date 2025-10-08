6463f479ca26eb1a088444a23ca202727d4e18cb
Wizzy019 <kingwizzyndukwu@gmail.com>
2025-10-08 20:16:54 +0100

    SetUp: for Deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
+import process from 'process'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
+export default defineConfig(({ mode }) => {
+  // Load all environment variables
+  const env = loadEnv(mode, process.cwd(), '')
+  const isVercel = env.VERCEL === '1'
+
+  return {
+    plugins: [react(), tailwindcss()],
+    base: isVercel ? '/' : '/Expense-Handler-App/',
+}
 })
