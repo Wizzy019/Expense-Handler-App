85a1a31575989f4de32e6a5201ea11b5b13b2f6c
Wizzy019 <kingwizzyndukwu@gmail.com>
2025-10-08 08:01:33 +0100

    Added an Expense Card to th UI to display Expense form and Expense List

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import ExpenseForm from './components/ExpenseForm'
 
 function App() {
   return (
-    < div className="App h-screen mx-auto p-4 max-w-2xlm bg-gray-300">
+    < div className="App h-screen w-auto mx-auto p-4 max-w-2xlm bg-gray-300">
       <h1 className="heading text-5xl font-bold mb-2.5 mx-15 my-10">
         Expense Handler
       </h1>
+      <div className="ExpenseCard w-max p-5 mt-5 rounded-2xl bg-white">
       <ExpenseForm/>
+      </div>
     </div>
   )
 }
